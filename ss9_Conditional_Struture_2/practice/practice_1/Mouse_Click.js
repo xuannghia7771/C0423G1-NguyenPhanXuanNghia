07ce05e336b8038e32651a614ce7c368a7279454
xuannghia <nghiaphan33dn@gmail.com>
2023-05-13 11:57:57 +0700

    upload bai tap ngay 12-05

---
+++ ss9_Conditional_Struture_2/practice/practice_1/Mouse_Click.js
@@ -0,0 +1,11 @@
+let imgObj = null;
+imgObj = document.getElementById('myImage');
+function init() {
+    imgObj = document.getElementById('myImage');
+    imgObj.style.position = 'relative';
+    imgObj.style.left = '0px';
+}
+function moveRight() {
+    imgObj.style.left = parseInt(imgObj.style.left) + 10 + 'px';
+}
+window.onload = init;
